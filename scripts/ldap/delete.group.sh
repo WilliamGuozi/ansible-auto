01d6a062ff031ccab4afde305a6dc7ec88fa20f2
guojx <guojx@htrader.cn>
2018-03-14 20:30:59 +0800

    update ldap

--- scripts/ldap/delete.group.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USERFILE=list.groups
-
-while read LINE; do 
-group=`echo $LINE | awk -F"|" '{print $1}'`
-ldapdelete -x -w "123456" -D 'cn=root,dc=htrader,dc=cn' "cn=$group,ou=Group,dc=htrader,dc=cn"
-
-done < $USERFILE
--- scripts/ldap/delete.user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USERFILE=list.users
-
-while read LINE; do 
-user=`echo $LINE | awk -F"|" '{print $1}'`
-ldapdelete -x -w "123456" -D 'cn=root,dc=htrader,dc=cn' "uid=$user,ou=People,dc=htrader,dc=cn"
-
-done < $USERFILE
